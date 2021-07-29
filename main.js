bf357c90f975a796a28cc36e6946311b86955e47
Raul Zepeda <88167761+raulzpda@users.noreply.github.com>
2021-07-29 17:54:55 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+alert("Bienvenidos a Snowpiercer");
+
+document.querySelector(".menu-btn").addEventListener("click", () => {
+  document.querySelector(".nav-menu").classList.toggle("show");
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards-libro', { delay: 900 });
+ScrollReveal().reveal('.news-cards', { delay: 900 });
+ScrollReveal().reveal('.cards-banner-net', { delay: 900 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 900 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 900 });
+
